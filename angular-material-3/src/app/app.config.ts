f1eb67d1e31d340c51b9b28ca1e6733de6053548
Admin <Admin@YPHP>
2025-10-05 14:32:37 -0700

    remove "provideAnimationsAsync" due to angular 20 is no longer support.

--- angular-material-3/src/app/app.config.ts
+++ angular-material-3/src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig } from "@angular/core";
 import { provideRouter, withPreloading, withViewTransitions } from "@angular/router";
 import { QuicklinkStrategy, quicklinkProviders } from "ngx-quicklink";
 import { routes } from "./app.routes";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+//import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withPreloading(QuicklinkStrategy), withViewTransitions({ skipInitialTransition: true })),
-    provideAnimationsAsync(),
+    /*provideAnimationsAsync(),*/
     quicklinkProviders,
   ],
 };

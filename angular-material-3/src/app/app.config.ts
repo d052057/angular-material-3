dda17a1f091fef238c12211ef961b89d3a39c133
Admin <Admin@YPHP>
2025-10-24 15:02:38 -0700

    update angular cli

--- angular-material-3/src/app/app.config.ts
+++ angular-material-3/src/app/app.config.ts
@@ -2,12 +2,10 @@ import { ApplicationConfig } from "@angular/core";
 import { provideRouter, withPreloading, withViewTransitions } from "@angular/router";
 import { QuicklinkStrategy, quicklinkProviders } from "ngx-quicklink";
 import { routes } from "./app.routes";
-//import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withPreloading(QuicklinkStrategy), withViewTransitions({ skipInitialTransition: true })),
-    /*provideAnimationsAsync(),*/
     quicklinkProviders,
   ],
 };
